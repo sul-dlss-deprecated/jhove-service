4c5bf9ec0206feb242ed2f49754e5b43fdf048fc
Peter Mangiafico <peter@mangiafico.org>
2017-03-10 11:07:58 -0800

    replicate the filelist functionality in ruby code for running on a list of files; update some tests

--- spec/lib/jhove_service_spec.rb
+++ spec/lib/jhove_service_spec.rb
@@ -13,6 +13,10 @@ describe JhoveService do
     @jhove_service = JhoveService.new(@temp.to_s)
   end
 
+  after :each do
+    @jhove_service.cleanup
+  end
+
   after :all do
     @temp.rmtree if @temp.exist?
   end
@@ -21,31 +25,40 @@ describe JhoveService do
     expect(@jhove_service.target_pathname).to eq @fixtures.join('temp')
   end
 
-  specify "JhoveService#get_jhove_command" do
+  it "produces the correct get_jhove_command" do
     jhove_cmd = @jhove_service.get_jhove_command(@content_dir)
-    expect(jhove_cmd).to eq "#{@bin.join('jhoveToolkit.sh')} -h xml -o #{@temp.join('jhove_output.xml')} #{@fixtures.join('test_files')}"
-    jhove_cmd = @jhove_service.get_jhove_command(@content_dir, "/my/fileset.txt")
-    expect(jhove_cmd).to eq "#{@bin.join('jhoveToolkit.sh')} -h xml -o #{@temp.join('jhove_output.xml')} edu.stanford.sulair.jhove.JhoveFileset #{@fixtures.join('test_files')} /my/fileset.txt"
+    expect(jhove_cmd).to eq "#{@bin.join('jhoveToolkit.sh')} -h xml -o \"#{@temp.join('jhove_output.xml')}\" \\\"#{@fixtures.join('test_files')}"
+    jhove_cmd = @jhove_service.get_jhove_command(@content_dir,'/some/custom/output.xml')
+    expect(jhove_cmd).to eq "#{@bin.join('jhoveToolkit.sh')} -h xml -o \"/some/custom/output.xml\" \\\"#{@fixtures.join('test_files')}"
   end
 
   it "can run jhove against a directory" do
     jhove_output = @jhove_service.run_jhove(@content_dir.join('audio'))
     jhove_xml = Nokogiri::XML(IO.read(jhove_output))
     expect(jhove_xml.root.name).to eq('jhove')
-    nodes = jhove_xml.xpath('//jhove:repInfo', 'jhove' => 'http://hul.harvard.edu/ois/xml/ns/jhove')
-    expect(nodes.size).to eq(2)
+    expect(count_nodes(jhove_xml)).to eq(2)
+    expect(count_errors(jhove_xml)).to eq(0)
   end
 
   it "can run jhove against a list of files in a directory" do
     jhove_output = @jhove_service.run_jhove(@content_dir, @fixtures.join('fileset.txt'))
     jhove_xml = Nokogiri::XML(IO.read(jhove_output))
     expect(jhove_xml.root.name).to eq('jhove')
-    nodes = jhove_xml.xpath('//jhove:repInfo', 'jhove' => 'http://hul.harvard.edu/ois/xml/ns/jhove')
-    expect(nodes.size).to eq(6)
+    files_in_set = 6
+    expect(count_nodes(jhove_xml)).to eq(files_in_set)
+    expect(count_errors(jhove_xml)).to eq(0)
+    for i in 0..files_in_set-1
+      expect(File.exists?(@jhove_service.target_pathname.join("jhove_output_#{i}.xml"))).to be_falsey # it cleans up the temp files it created
+    end
   end
 
-  it "should raise an exception if directory does not exist" do
-     expect(lambda{@jhove_service.run_jhove('/temp/dummy/@#')}).to raise_exception(%r{Folder /temp/dummy/@# not found})
+  it "should raise an exception if directory or file list passed to run_jhove does not exist" do
+     expect(lambda{@jhove_service.run_jhove('/temp/dummy/@#')}).to raise_exception(%r{Content /temp/dummy/@# not found})
+     expect(lambda{@jhove_service.run_jhove(@content_dir,'/my/fileset.txt')}).to raise_exception(%r{File list /my/fileset.txt not found})
+  end
+
+  it "should raise an exception if the filelist exists but has no files in it" do
+     expect(lambda{@jhove_service.run_jhove(@content_dir,@fixtures.join('empty_fileset.txt'))}).to raise_exception("File list #{@fixtures.join('empty_fileset.txt')} empty")
   end
 
   it "can create technical metadata" do
@@ -99,6 +112,8 @@ describe JhoveService do
 
 
   it "can do cleanup" do
+    File.open(@jhove_service.jhove_output, "w") {}
+    File.open(@jhove_service.tech_md_output, "w") {}
     expect(File.exist?(@jhove_service.jhove_output)).to be_truthy
     expect(File.exist?(@jhove_service.tech_md_output)).to be_truthy
     @jhove_service.cleanup
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,14 @@ require 'rspec'
 require 'rspec/matchers' # req by equivalent-xml custom matcher `be_equivalent_to`
 require 'equivalent-xml'
 
+def count_nodes(jhove_xml)
+  jhove_xml.xpath('//jhove:repInfo', 'jhove' => 'http://hul.harvard.edu/ois/xml/ns/jhove').size
+end
+
+def count_errors(jhove_xml)
+  jhove_xml.css('//repInfo/messages/message[@severity="error"]').size
+end
+
 RSpec.configure do |config|
 
 end
